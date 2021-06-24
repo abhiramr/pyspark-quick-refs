cb111336cf2121218be400813e7ebfca1ee034e4
Abhiram R <abhi.darkness@gmail.com>
2021-06-24 09:48:45 +0530

    Update misc_csv.py

--- misc_csv.py
+++ misc_csv.py
@@ -1,3 +1,8 @@
+# Read Parquet
+df = spark.read.option("header", "true").option("inferSchema", "false").parquet("./Desktop/000.parquet")
+df.printSchema()
+
+
 # Read CSV
-df = spark.read.option("header", "true").option("inferSchema", "false").parquet("./Desktop/000.csv")
+df = spark.read.option("header", "true").option("inferSchema", "false").csv("./Desktop/000.csv")
 df.printSchema()
